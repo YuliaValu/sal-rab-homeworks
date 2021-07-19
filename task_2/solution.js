1a5171ad7c075cbb30ab75132363052789536b78
Yulia Valujskaja <valuiskaia89@gmail.com>
2021-07-19 10:58:09 +0300

    Task 2 done 13

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,7 +15,7 @@ function calcShipping(sum, min, shipping) {
     if(productsSum >= freeShippingMinSum) { shippingSum = 0 }
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    if( productsSum > 0 && productsSum < freeShippingMinSum) {shippingSum = shippingPrice }
+    if( productsSum > 0 || productsSum < freeShippingMinSum) {shippingSum = shippingPrice }
 
     // Конец решения задания №2.1.
 
@@ -30,11 +30,10 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountPrice
-    let discountPrice;
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountPrice значение discountPart процентов от productsSum,
     // иначе присвойте discountPrice значения 0
-    discountPrice = productsSum >=discountMinSum ? productsSum/100*discountPart : 0;
+    let discountPrice = productsSum >=discountMinSum ? productsSum/100*discountPart : 0;
 
     // Конец решения задания №2.2.
 
