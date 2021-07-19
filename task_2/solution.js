f9ad544013c21d99708881d96cb58206009127f9
Yulia Valujskaja <valuiskaia89@gmail.com>
2021-07-19 20:00:38 +0300

    Task 2 done 24

--- task_2/solution.js
+++ task_2/solution.js
@@ -12,10 +12,10 @@ function calcShipping(sum, min, shipping) {
     if (productsSum == 0) { shippingSum = 0 }
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-    if productsSum >= freeShippingMinSum) { shippingSum = 0 }
+    if (productsSum >= freeShippingMinSum) { shippingSum = 0 }
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    if( productsSum > 0 && productsSum < freeShippingMinSum) {shippingSum = shippingPrice }
+    if( productsSum > 0 || productsSum < freeShippingMinSum) {shippingSum = shippingPrice }
 
     // Конец решения задания №2.1.
 
