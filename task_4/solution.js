542709b55dc30f0579c206a573d4721061ce2710
Yulia Valujskaja <valuiskaia89@gmail.com>
2021-07-20 18:20:55 +0300

    Task 4 v2

--- task_4/solution.js
+++ task_4/solution.js
@@ -8,7 +8,7 @@ function parseProducts(json) {
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
 }
-let data = JSON.parse(json);
+data = JSON.parse(json);
 products = data.products;
 
 // Напишите функцию renderProductsCards(json)
