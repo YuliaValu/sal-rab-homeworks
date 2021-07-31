04c944cb752409022f0a43f500654ea8641bebf9
Yulia Valujskaja <valuiskaia89@gmail.com>
2021-07-31 22:44:20 +0300

    task_final 6

--- task_final/solution.js
+++ task_final/solution.js
@@ -50,7 +50,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.client = 'Иван';
 
-    let jsonData = JSON.stringify({data});
+    let jsonData = JSON.stringify(data);
 
     return jsonData;
 }
