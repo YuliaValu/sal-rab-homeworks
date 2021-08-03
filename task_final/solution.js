085032b4a2ef42297796a8d9b4865e7bcf73858d
Yulia Valujskaja <valuiskaia89@gmail.com>
2021-08-03 23:02:39 +0300

    task_final 15

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,19 +33,19 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let client = name + " " + phone;
-    let addressForSend= "ул." + addres.street + ", дом" + address.house + "," + address.entrance + "подьезд," + address.floor + " этаж, кв" + address.flat; 
-    let order = {address : addressForSend, sum:sum };
-    let goodsForSend = [];
-    let countOfGoods = goods.length;
-    for (let i = 0; i < countOfGoods; i ++) {
-        let goods = { title: goods[i].title, count:goods[i].count };
-        goodsForSend.push(goods);
-    }
-    let data = {client:client, order:order,goods:goodsForSend };
-    let dataForSend= {data:data};
-    let jsonData = JSON.stringify(dataForSend);
-    return jsonData;
-    }
-    
+
+    function sendRequest(name, phone, address, goods, sum) { 
+        let client = name + " " + phone; 
+        let addressSendForClient = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
+        let order = {address : addressSendForClient, sum:sum }; 
+        let goodsSendForClient = []; 
+        let countOfGoods = goods.length; 
+        for (let i = 0; i < countOfGoods; i ++) { 
+            let x = { title: goods[i].title, count:goods[i].count }; 
+            goodsSendForClient.push(x); 
+        } 
+        let data = {client:client, order:order,goods:goodsSendForClient }; 
+        let dataForSend= {data:data}; 
+        let jsonData = JSON.stringify(dataForSend); 
+        return jsonData; 
+        }
